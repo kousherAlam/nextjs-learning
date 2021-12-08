5544b97bc54cc7837fc496a2d6ebead8c8d6dc3a
Kousher Alam <kousheralampranto@gmail.com>
2021-12-08 23:27:14 +0600

    updted everything into typescript

--- lib/posts.ts
+++ lib/posts.ts
@@ -6,14 +6,23 @@ import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
+export interface MatterResult {
+  id: string;
+  content: string;
+  data: {
+    title: string;
+    date: string;
+  },
+  isEmpty: boolean;
+  excerpt: any;
+}
+
+
 export function getSortedPostsData() {
-  // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory)
   const allPostsData = fileNames.map(fileName => {
     // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, '')
-
-    // Read markdown file as string
     const fullPath = path.join(postsDirectory, fileName)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
 
@@ -27,7 +36,7 @@ export function getSortedPostsData() {
     }
   })
   // Sort posts by date
-  return allPostsData.sort((a, b) => {
+  return allPostsData.sort((a: any, b: any): any => {
     if (a.date < b.date) {
       return 1
     } else {
@@ -47,7 +56,7 @@ export function getAllPostIds() {
   })
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
