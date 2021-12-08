ed7d672edfc926b75213a94ba0e35b532a1b0506
Kousher Alam <kousheralampranto@gmail.com>
2021-12-08 22:29:48 +0600

    added tailwnd css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/global.scss';
 import { AppProps } from 'next/app';
 
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
