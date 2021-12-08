4f54b04ee4594e562f3e5fb9a9e67997f0c15c6f
Kousher Alam <kousheralampranto@gmail.com>
2021-12-08 22:21:50 +0600

    updated to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData }: any) {
   return (
     <Layout home>
       <Head>
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({ id, date, title }: any) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,10 +1,11 @@
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head'
-import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next';
 
-export default function Post({ postData }) {
+export default function Post({ postData }: any) {
   return (
     <Layout>
       <Head>
@@ -21,7 +22,7 @@ export default function Post({ postData }) {
   )
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async() => {
   const paths = getAllPostIds()
   return {
     paths,
@@ -29,7 +30,7 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }: any) => {
   const postData = await getPostData(params.id)
   return {
     props: {
